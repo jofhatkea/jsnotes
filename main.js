63dcae4d7845d8aa6384c62b32f57b2050843e80
Jonas Holbech <jh@jonasholbech.dk>
2018-06-05 14:41:44 +0200

    edit done

--- main.js
+++ main.js
@@ -30,6 +30,34 @@ database.ref("notes/").on("child_added", (snapshot)=>{
     clone.querySelector("button.delete").addEventListener("click", e=>{
         database.ref("notes/"+key).remove();
     });
+    clone.querySelector("button.edit").addEventListener("click", e=>{
+        const tempTemplate = document.querySelector("#editFormTemplate").content;
+        const formClone = tempTemplate.cloneNode(true);
+        const theArticle = e.target.parentElement.parentElement;
+        const theDiv = theArticle.querySelector("div").textContent;
+        const theHeader = theArticle.querySelector("h1").textContent;
+        const theKey = theArticle.dataset.key;
+        console.log(theDiv, theHeader, theKey);
+        formClone.querySelector("input").value=theHeader;
+        formClone.querySelector("textarea").value=theDiv;
+        formClone.querySelector("form").addEventListener("submit",e=>{
+            e.preventDefault();
+            let lastForm = document.querySelector("footer+form");
+            console.log(lastForm)
+            database.ref("notes/"+theKey).set({
+                header: lastForm.querySelector("input").value,
+                description: lastForm.querySelector("textarea").value
+            });
+            
+            theArticle.querySelector("h1").textContent=lastForm.querySelector("input").value;
+            theArticle.querySelector("div").textContent=lastForm.querySelector("textarea").value;
+            lastForm.remove()
+            //window.location="" // works, not nice
+            //we should prob use child_changed
+
+        });
+        theArticle.appendChild(formClone);
+    });
     app.appendChild(clone);
 
 });
